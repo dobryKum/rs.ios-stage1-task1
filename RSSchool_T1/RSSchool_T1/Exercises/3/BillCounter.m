611ffc4f2ffbeba479d429d0649ed2ca41825f00
TimofeyZykov <zikovtm@gmail.com>
2020-03-16 21:38:35 +0300

    refactor: add 3 and 4 tasks

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,23 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray *newBill = [NSMutableArray arrayWithArray:bill];
+    [newBill removeObjectAtIndex:index];
+    NSInteger newSum = sum.intValue;
+    
+    NSInteger sumWithoutAnna = 0;
+    for (int i = 0; i < [newBill count]; i++) {
+        sumWithoutAnna += [newBill[i] intValue];
+    }
+    
+    NSInteger billForAnna = sumWithoutAnna / 2;
+    
+    if (newSum > billForAnna) {
+        NSInteger finalSum = newSum - billForAnna;
+        return [NSString stringWithFormat:@"%ld", (long)finalSum];
+    } else {
+        return @"Bon Appetit";
+    }
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,44 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray *openBracketsArray = [NSMutableArray new];
+    NSMutableArray *closeBracketsArray = [NSMutableArray new];
+    NSMutableArray<NSString*> *result = [NSMutableArray new];
+    for (int i = 0; i < [string length]; i++) {
+        char character = [string characterAtIndex:i];
+        if ( character == '(' || character == '[' || character == '<' ) {
+            [openBracketsArray addObject:@[[NSString stringWithFormat:@"%c", character],@(i)]];
+        }
+        if ( character == ')' || character == ']' || character == '>' ) {
+            [closeBracketsArray addObject:@[[NSString stringWithFormat:@"%c", character],@(i)]];
+        }
+    }
+    
+    for (int i = 0; i < [openBracketsArray count]; i++) {
+        for (int j = 0; j < [closeBracketsArray count]; j++) {
+//            NSInteger test1 = [openBracketsArray[i][1] intValue];
+//            NSInteger test2 = [closeBracketsArray[j][1] intValue];
+            if ([openBracketsArray[i][0] isEqualToString:@"("] && [closeBracketsArray[j][0] isEqualToString:@")"]) {
+                NSRange range = NSMakeRange([openBracketsArray[i][1] intValue]+1, [closeBracketsArray[j][1] intValue]-[openBracketsArray[i][1] intValue]-1);
+                [result addObject:[string substringWithRange:range]];
+                [closeBracketsArray removeObjectAtIndex:j];
+                break;
+            } else if ([openBracketsArray[i][0] isEqualToString:@"["] && [closeBracketsArray[j][0] isEqualToString:@"]"]) {
+                NSRange range = NSMakeRange([openBracketsArray[i][1] intValue]+1, [closeBracketsArray[j][1] intValue]-[openBracketsArray[i][1] intValue]-1);
+                [result addObject:[string substringWithRange:range]];
+                [closeBracketsArray removeObjectAtIndex:j];
+                break;
+            } else if ([openBracketsArray[i][0] isEqualToString:@"<"] && [closeBracketsArray[j][0] isEqualToString:@">"]) {
+                NSRange range = NSMakeRange([openBracketsArray[i][1] intValue]+1, [closeBracketsArray[j][1] intValue]-[openBracketsArray[i][1] intValue]-1);
+                [result addObject:[string substringWithRange:range]];
+                [closeBracketsArray removeObjectAtIndex:j];
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+    return result;
 }
 
 @end
