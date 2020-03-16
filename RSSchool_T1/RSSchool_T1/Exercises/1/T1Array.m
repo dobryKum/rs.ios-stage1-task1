8dbb8913a93707fa533c956c5f2e62772e82c4cd
TimofeyZykov <zikovtm@gmail.com>
2020-03-16 13:16:22 +0300

    refactor: complete 1 and 2 tasks

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,20 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *happyArray = [NSMutableArray arrayWithArray:sadArray];
+    if ([happyArray count] == 0) {
+        return happyArray;
+    }
+    for (int i = 1; i < [happyArray count] - 1; i++) {
+        NSNumber *middleNumber = happyArray[i];
+        NSNumber *leftNumber = happyArray[i-1];
+        NSNumber *rightNumber = happyArray[i+1];
+        if (middleNumber.intValue >= leftNumber.intValue + rightNumber.intValue) {
+            [happyArray removeObjectAtIndex:i];
+            i = 0;
+        }
+    }
+    return happyArray;
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,22 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    NSMutableArray *newArray = [NSMutableArray arrayWithArray:array];
+    NSInteger sum = [newArray[0] intValue];
+    NSNumber *maxValue = newArray[0];
+    NSNumber *minValue = newArray[0];
+    for (int i = 1; i < [newArray count]; i++) {
+        sum += [newArray[i] intValue];
+        if ([minValue intValue] > [newArray[i] intValue]) {
+            minValue = newArray[i];
+        }
+        if ([maxValue intValue] < [newArray[i] intValue]) {
+            maxValue = newArray[i];
+        }
+    }
+    NSInteger maxSum = sum - [minValue intValue];
+    NSInteger minSum = sum - [maxValue intValue];
+    return @[@(minSum), @(maxSum)];
 }
 
 @end
